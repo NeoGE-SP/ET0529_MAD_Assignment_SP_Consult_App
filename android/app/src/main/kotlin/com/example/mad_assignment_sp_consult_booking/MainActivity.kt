e21bd3353f3798451ba542e104d7f2704f203045
Neo Gao En <neoge07.24@ichat.sp.edu.sg>
2026-01-23 13:41:43 +0800

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/mad_assignment_sp_consult_booking/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mad_assignment_sp_consult_booking
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:mad_assignment_sp_consult_booking/login.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Login(),
+    );
+  }
+}
